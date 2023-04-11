1d8e5b86744bba7dfd049e7d3a52f136636afe91
Franklin Sumner <sumnerfranklinm@gmail.com>
2023-04-11 16:52:24 -0400

    added apollo server

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
@@ -21,7 +21,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-  uri: '/graphql',
+  uri: authLink.contact(httpLink),
   cache: new InMemoryCache(),
 });
 
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,17 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-
+        me: async(parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user_id})
+                .select('-__v -password')
+                return userData;
+            }
+            throw new AuthenticationError('Please log in first')
+        }
     },
 
     Mutation: {
